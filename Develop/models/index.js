d4c747621fb3f42e16f4b99446317b087a475ef5
Calvin Swomley <calvinswomley@gmail.com>
2021-12-04 18:07:03 -0700

    Index model and category model

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, { //cjs
+  foreignKey: 'category_id',  //cjs
+});   //cjs
 // Categories have many Products
-
+Category.hasMany(Products, {   //cjs
+  foreignKey: 'category_id',   //cjs
+  onDelete: 'CASCADE',   //cjs
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {   //cjs
+  through: {   //cjs
+    model: ProductTag,   //cjs
+  },   //cjs
+})   //cjs
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {   //cjs
+  through: {   //cjs
+    model: ProductTag,   //cjs
+  },   //cjs
+})    //cjs
 
 module.exports = {
   Product,
